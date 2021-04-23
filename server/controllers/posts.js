0f0fc7d07df0908701effb2571ed311d0e3c9d63
Shalaw Fatah <shalaw.fatah@gmail.com>
2021-04-23 15:23:51 -0700

    update, delete, view, create added

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -25,10 +25,37 @@ export const createPost = async (req, res) => {
     }
 }
 
-export const deletePost = (req, res) => {
-    res.send('Delete One Post')
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await Post.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
 }
 
-export const updatePost = (req, res) => {
-    res.send('Update One Post')
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await Post.findByIdAndRemove(id);
+
+    res.json({ message: "Post deleted successfully." });
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await Post.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    
+    res.json(updatedPost);
 }
--- server/index.js
+++ server/index.js
@@ -6,10 +6,9 @@ dotenv.config()
 
 import PostRoutes from './routes/posts.js'
 const app = express()
-
 app.use(cors())
-app.use(express.json())
-
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use('/posts', PostRoutes)
 
 database()
